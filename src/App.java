c89f9c562d5b24e3e694b3105e9e418bae40bb35
modengann <99417989+modengann@users.noreply.github.com>
2022-11-22 14:44:22 -0500

    init

---
+++ src/App.java
@@ -0,0 +1,17 @@
+/** NOTE: THIS SPACE WILL BE UPDATED. ONLY FOR PEOPLE WHO WANT TO WORK OVER THANKSGIVING
+ *  
+ *  
+ */
+
+
+import javadraw.*;
+
+public class App extends Window{
+    public static void main(String[] args) throws Exception {
+        Window.open();
+    }
+
+    public void start(){
+
+    }
+}
